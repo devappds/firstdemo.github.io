39b75838238a9c9d5bcbe1ef6c5f9531bf039ac8
Dipa Pansuriya <138754218+devappds@users.noreply.github.com>
2023-07-11 11:19:52 +0530

    Add files via upload

---
+++ App (1).js
@@ -0,0 +1,111 @@
+import "./App.css";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+function App() {
+  return (
+    <div className="container">
+      <OwlCarousel className="owl-theme" loop margin={10} nav>
+        <div class="item">
+          <div class="shadow-effect">
+            <img
+              class="img-circle"
+              src="https://cloudwebprojects.com/demo/businessitsupport/wp-content/uploads/2023/05/67a0824a-9253-4352-80a3-07c29840c665.jpg"
+            />
+            <p>
+              orem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div class="testimonial-name">
+            <h5>John</h5>
+            <small>Test address</small>
+          </div>
+        </div>
+        <div class="item">
+          <div class="shadow-effect">
+            <img
+              class="img-circle"
+              src="https://cloudwebprojects.com/demo/businessitsupport/wp-content/uploads/2023/05/67a0824a-9253-4352-80a3-07c29840c665.jpg"
+            />
+            <p>
+              orem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div class="testimonial-name">
+            <h5>John</h5>
+            <small>Test address</small>
+          </div>
+        </div>
+        <div class="item">
+          <div class="shadow-effect">
+            <img
+              class="img-circle"
+              src="https://cloudwebprojects.com/demo/businessitsupport/wp-content/uploads/2023/05/67a0824a-9253-4352-80a3-07c29840c665.jpg"
+            />
+            <p>
+              orem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div class="testimonial-name">
+            <h5>John</h5>
+            <small>Test address</small>
+          </div>
+        </div>
+        <div class="item">
+          <div class="shadow-effect">
+            <img
+              class="img-circle"
+              src="https://cloudwebprojects.com/demo/businessitsupport/wp-content/uploads/2023/05/67a0824a-9253-4352-80a3-07c29840c665.jpg"
+            />
+            <p>
+              orem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div class="testimonial-name">
+            <h5>John</h5>
+            <small>Test address</small>
+          </div>
+        </div>
+        <div class="item">
+          <div class="shadow-effect">
+            <img
+              class="img-circle"
+              src="https://cloudwebprojects.com/demo/businessitsupport/wp-content/uploads/2023/05/67a0824a-9253-4352-80a3-07c29840c665.jpg"
+            />
+            <p>
+              orem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div class="testimonial-name">
+            <h5>John</h5>
+            <small>Test address</small>
+          </div>
+        </div>
+        <div class="item">
+          <div class="shadow-effect">
+            <img
+              class="img-circle"
+              src="https://cloudwebprojects.com/demo/businessitsupport/wp-content/uploads/2023/05/67a0824a-9253-4352-80a3-07c29840c665.jpg"
+            />
+            <p>
+              orem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div class="testimonial-name">
+            <h5>John</h5>
+            <small>Test address</small>
+          </div>
+        </div>
+      </OwlCarousel>
+      ;
+    </div>
+  );
+}
+
+export default App;
